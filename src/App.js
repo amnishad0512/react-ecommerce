b26cf7626e31ab40de5f83b6cf8e8a43faba9b49
Manoj <amnishad0512@gmail.com>
2022-09-27 16:17:56 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contact from "./Contact";
 import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 
 const App = () => {
   const theme = {
@@ -33,6 +34,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyle />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
